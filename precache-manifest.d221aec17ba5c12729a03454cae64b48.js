abc3eaed7a2947eaffb3cd780761227f34f93991
DanyalJanjua <daniafzal29@gmail.com>
2019-08-20 21:00:51 +0500

    Updates

---
+++ precache-manifest.d221aec17ba5c12729a03454cae64b48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a224f092e86d0c7cb5deb3b7047e9962",
+    "url": "/lyrics/index.html"
+  },
+  {
+    "revision": "622f030cda2dbd9299e6",
+    "url": "/lyrics/static/css/main.40fcde07.chunk.css"
+  },
+  {
+    "revision": "5663bdd834c08708af64",
+    "url": "/lyrics/static/js/2.f9c88aa1.chunk.js"
+  },
+  {
+    "revision": "622f030cda2dbd9299e6",
+    "url": "/lyrics/static/js/main.22fe6af7.chunk.js"
+  },
+  {
+    "revision": "f6df9479cc61ecd70db5",
+    "url": "/lyrics/static/js/runtime~main.012b03e0.js"
+  },
+  {
+    "revision": "819ff6381aef473b51076cf8336afb69",
+    "url": "/lyrics/static/media/slide_one.819ff638.jpg"
+  },
+  {
+    "revision": "75a7fe39896dc517b9b437a4328f04c9",
+    "url": "/lyrics/static/media/slide_three.75a7fe39.jpg"
+  },
+  {
+    "revision": "13f57d76e008be5bb4df84244b1e1369",
+    "url": "/lyrics/static/media/slide_two.13f57d76.jpg"
+  }
+]);
